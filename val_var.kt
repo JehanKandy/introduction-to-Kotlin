246986dfdd0d2d1c31fd3eab92402b508c8189fb
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-12-17 23:13:13 +0530

    Add files via upload

---
+++ val_var.kt
@@ -0,0 +1,27 @@
+//introduction to Kotlin Programming
+
+fun main(){
+    //this is a comment
+    
+    //val --> read-only    
+    val fn = "Jehan \n"
+    //\n mean leave a line
+    print(fn)
+
+    //var --> read and write
+    var number = 10
+    print("$number \n")
+
+    //Double
+    val jkdouble: Double = 2.5
+    print(jkdouble)
+
+    //Boolean
+    val jkboolt:Boolean = true
+    val jkboolf:Boolean = false
+
+    print("\n $jkboolt, $jkboolf")
+    
+
+
+}
